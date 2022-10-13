1bde3c530c348a72dfbda2404235381ecc711f65
Goularte G.C <92948398+ggoulartec@users.noreply.github.com>
2022-10-13 12:24:48 -0300

    Create index.php

---
+++ backend/index.php
@@ -0,0 +1,47 @@
+<?php
+
+$data_inicial = isset($_POST['data_inicial']) ? $_POST['data_inicial'] : false;
+$intevalo_ciclos = isset($_POST['intevalo_ciclos']) ? $_POST['intevalo_ciclos'] : false;
+$quantidade_ciclos = isset($_POST['quantidade_ciclos']) ? $_POST['quantidade_ciclos'] : false;
+
+if (!$data_inicial || !$intevalo_ciclos || !$quantidade_ciclos) {
+    $data['status'] = false;
+    $data['message'] = "Preencha todos os campos";
+    echo json_encode($data);
+    exit;
+}
+
+
+function generateDate($data_inicial, $nu_ciclos, $nu_prazo)
+{
+    $diasemana = array('Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado');
+
+    $date = new DateTime($data_inicial);
+    $ciclo = new DateInterval('P' . $nu_ciclos . 'D');
+    $prazo = new DatePeriod($date, $ciclo, $nu_prazo);
+
+    $datas = array();
+
+    foreach ($prazo as $data) {
+        $diasemana_numero  = date('w', strtotime($data->format('Y-m-d')));
+        $dados = array();
+        $dados["datas_sem_acrecimos"] = $data->format('Y-m-d');
+
+        if ($diasemana[$diasemana_numero] == "Sabado") :
+            $newDate = new DateTime($dados["datas_sem_acrecimos"]);
+            $newDate->add(new DateInterval('P2D'));
+            $dados["datas_acrecimos"] = $newDate->format('Y-m-d');
+        endif;
+
+        if ($diasemana[$diasemana_numero] == "Domingo") :
+            $newDate = new DateTime($dados["datas_sem_acrecimos"]);
+            $newDate->add(new DateInterval('P1D'));
+            $dados["datas_acrecimos"] = $newDate->format('Y-m-d');
+        endif;
+
+        $datas[] = $dados;
+    }
+    return $datas;
+};
+
+echo json_encode(generateDate($data_inicial, $intevalo_ciclos, $quantidade_ciclos));
