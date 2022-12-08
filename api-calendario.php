738d87c84b6913be3293b17dda223881ee5d15fc
Goularte G.C <92948398+ggoulartec@users.noreply.github.com>
2022-12-08 15:07:45 -0300

    adicionado api feriados

--- api-calendario.php
+++ api-calendario.php
@@ -55,3 +55,19 @@ function generateDate($data_inicial, $nu_ciclos, $nu_prazo)
 };
 
 echo json_encode(generateDate($data_inicial, $intevalo_ciclos, $quantidade_ciclos));
+
+$curl = curl_init();
+
+//curl_setopt_array($curl, array(
+//    CURLOPT_URL => 'https://api.invertexto.com/v1/holidays/2022?token=2076%7CTe1NXEbrla6sEFN7nH2wcmE1FJH8u71K&state=PR',
+//    CURLOPT_RETURNTRANSFER => true,
+//    CURLOPT_ENCODING => '',
+//    CURLOPT_MAXREDIRS => 10,
+//    CURLOPT_TIMEOUT => 0,
+//    CURLOPT_FOLLOWLOCATION => true,
+//    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//    CURLOPT_CUSTOMREQUEST => 'GET',
+//));
+//
+//$response = curl_exec($curl);
+//curl_close($curl);
