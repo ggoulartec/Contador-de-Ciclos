4ea18fb43bce38deefd6ee0753fd4e608639d468
goularti <sistemas4@leiloesjudiciais.com.br>
2025-05-15 13:35:08 -0300

    update mixed links.

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
- function url(){
+function url()
+{
     return sprintf(
-      "%s://%s%s",
-      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-      $_SERVER['SERVER_NAME'],
-      $_SERVER['REQUEST_URI']
+        "%s://%s%s",
+        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+        $_SERVER['SERVER_NAME'],
+        $_SERVER['REQUEST_URI']
     );
-  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,149 +17,152 @@
 <head>
     <meta charset="UTF-8">
     <title>Calendario</title>
-    <link rel="stylesheet" href="<?=url()?>/assets/css/dycalendar.css">
-    <link rel="stylesheet" href="<?=url()?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?= url() ?>/assets/css/dycalendar.css">
+    <link rel="stylesheet" href="<?= url() ?>/assets/css/style.css">
     <style>
         .dycalendar-target-date-red {
             background: red;
-            color     : #FFF !important;
+            color: #FFF !important;
         }
     </style>
 </head>
 
 <body>
-    <div class="box2">
-        <div class="container-lista">
-            <ul class="form lista none" style="overflow-x: auto;"></ul>
-            <a href="#" id="close" class="btn"><span>voltar</span></a>
-        </div>
-        <form class="form">
-            <h2>Contador de Ciclos</h2>
-            <div class="inputBox">
-                <input type="date" name="data_inicial" required="required" style="z-index: 1">
-                <span style="z-index: 1">Data de inicio do prazo</span>
-                <em></em>
-            </div>
-            <div class="inputBox">
-                <input type="number" name="intevalo_ciclos" required="required">
-                <span>Intervalo entre os cliclos</span>
-                <em></em>
-            </div>
-            <div class="inputBox">
-                <input type="number" name="quantidade_ciclos" required="required" min="1">
-                <span>Quantidade de Ciclos</span>
-                <em></em>
-            </div>
-            <input type="submit" value="Gerar datas">
-        </form>
+<div class="box2">
+    <div class="container-lista">
+        <ul class="form lista none" style="overflow-x: auto;"></ul>
+        <a href="#" id="close" class="btn"><span>voltar</span></a>
     </div>
-    <section>
-        <div class="box">
-            <div class="container">
-                <div id="dycalendar"></div>
-            </div>
+    <form class="form">
+        <h2>Contador de Ciclos</h2>
+        <div class="inputBox">
+            <input type="date" name="data_inicial" required="required" style="z-index: 1">
+            <span style="z-index: 1">Data de inicio do prazo</span>
+            <em></em>
         </div>
-    </section>
+        <div class="inputBox">
+            <input type="number" name="intevalo_ciclos" required="required">
+            <span>Intervalo entre os cliclos</span>
+            <em></em>
+        </div>
+        <div class="inputBox">
+            <input type="number" name="quantidade_ciclos" required="required" min="1">
+            <span>Quantidade de Ciclos</span>
+            <em></em>
+        </div>
+        <input type="submit" value="Gerar datas">
+    </form>
+</div>
+<section>
+    <div class="box">
+        <div class="container">
+            <div id="dycalendar"></div>
+        </div>
+    </div>
+</section>
 
-    <script src="assets/js/dycalendar.js"></script>
+<script src="assets/js/dycalendar.js"></script>
 
-    <script>
-        (async () => {
-            const urlbase= '<?=url()?>';
+<script>
+    (async () => {
+        const urlbase = '<?=url()?>';
 
-            dycalendar.draw({ target: '#dycalendar', type: 'month', dayformat: 'full', prevnextbutton: 'show' })
+        dycalendar.draw({target: '#dycalendar', type: 'month', dayformat: 'full', prevnextbutton: 'show'})
 
-            const calendario = document.querySelector("#dycalendar");
-            const formulario = document.querySelector('form');
+        const calendario = document.querySelector("#dycalendar");
+        const formulario = document.querySelector('form');
 
-            const validacaoData = (dt) => {
-                const validacao         = ['D', 'S', 'T', 'Q']
-                const tdAll             = document.querySelectorAll('tbody tr td');
-                const calendario_header = document.querySelector('.dycalendar-header');
-                const options           = JSON.parse(calendario_header.getAttribute("data-option"));
-                const tds               = Array.from(tdAll).filter((td) => (!validacao.includes(td.innerText) && td.innerText.trim()))
+        const validacaoData = (dt) => {
+            const validacao = ['D', 'S', 'T', 'Q']
+            const tdAll = document.querySelectorAll('tbody tr td');
+            const calendario_header = document.querySelector('.dycalendar-header');
+            const options = JSON.parse(calendario_header.getAttribute("data-option"));
+            const tds = Array.from(tdAll).filter((td) => (!validacao.includes(td.innerText) && td.innerText.trim()))
 
-                const dia = dt.split('-')[2];
-                const mes = dt.split('-')[1];
-                const ano = dt.split('-')[0];
+            const dia = dt.split('-')[2];
+            const mes = dt.split('-')[1];
+            const ano = dt.split('-')[0];
 
-                const data = tds.map(element => {
-                    if (element.innerText.padStart(2, '0') == dia && (options.month + 1) == mes && options.year == ano) {
-                        return element;
-                    } else { return false; }
-                }).filter(data => data);
+            return tds.map(element => {
+                if (element.innerText.padStart(2, '0') === dia && (options.month + 1) === mes && options.year === ano) {
+                    return element;
+                } else {
+                    return false;
+                }
+            }).filter(data => data);
+        }
 
-                return data;
-            }
-
-            const marcacao = (person) => {
-                person.forEach((data) => {
-                    if (Object.keys(data).length == 2) {
-                        for (let index = 0; index < Object.keys(data).length; index++) {
-                            if (Object.keys(data)[index] == "datas_acrecimos") {
-                                const element = validacaoData(data.datas_acrecimos);
-                                if (element.length != 0) element[0].classList.add("dycalendar-target-date-red");
-                            }
+        const marcacao = (person) => {
+            person.forEach((data) => {
+                if (Object.keys(data).length == 2) {
+                    for (let index = 0; index < Object.keys(data).length; index++) {
+                        if (Object.keys(data)[index] == "datas_acrecimos") {
+                            const element = validacaoData(data.datas_acrecimos);
+                            if (element.length != 0) element[0].classList.add("dycalendar-target-date-red");
                         }
-                    } else {
-                        const element = validacaoData(data.datas_sem_acrecimos);
-                        if (element.length != 0) element[0].classList.add("dycalendar-target-date");
                     }
-                })
+                } else {
+                    const element = validacaoData(data.datas_sem_acrecimos);
+                    if (element.length != 0) element[0].classList.add("dycalendar-target-date");
+                }
+            })
+        }
+
+        formulario.addEventListener('submit', async event => {
+            event.preventDefault();
+
+            const data = new FormData(formulario);
+            const urlbase = window.location.origin + "/cciclos"
+            const response = await fetch(urlbase + "/api-calendario.php", {method: "post", body: data});
+            const person = await response.json();
+            const lista = document.querySelector('.lista');
+            const close = document.querySelector("#close");
+
+            if (person.status == false) {
+                alert(person.message)
             }
 
-            formulario.addEventListener('submit', async event => {
-                event.preventDefault();
+            formulario.classList.add('none');
+            lista.classList.remove('none');
+            close.classList.remove('none');
+            lista.innerHTML = "";
 
-                const data     = new FormData(formulario);
-                const response = await fetch(urlbase + "/api-calendario.php", { method: "post", body: data });
-                const person   = await response.json();
-                const lista    = document.querySelector('.lista');
-                const close    = document.querySelector("#close");
+            person.forEach(data => {
+                const dia = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[2] : data.datas_sem_acrecimos.split('-')[2];
+                const mes = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[1] : data.datas_sem_acrecimos.split('-')[1];
+                const ano = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[0] : data.datas_sem_acrecimos.split('-')[0];
+                const li = document.createElement('li');
+                li.innerText = "Data: " + `${dia}-${mes}-${ano}`;
 
-                if (person.status == false) {
-                    alert(person.message)
+                if (!data.datas_acrecimos) {
+                    li.classList.add('white');
+                } else {
+                    li.classList.add('red');
                 }
 
-                formulario.classList.add('none');
-                lista.classList.remove('none');
-                close.classList.remove('none');
-                lista.innerHTML = "";
+                lista.append(li);
+            })
 
-                person.forEach(data => {
-                    const dia    = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[2] : data.datas_sem_acrecimos.split('-')[2];
-                    const mes    = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[1] : data.datas_sem_acrecimos.split('-')[1];
-                    const ano    = (data.datas_acrecimos) ? data.datas_acrecimos.split('-')[0] : data.datas_sem_acrecimos.split('-')[0];
-                    const li     = document.createElement('li');
-                    li.innerText = "Data: " + `${dia}-${mes}-${ano}`;
+            marcacao(person);
 
-                    if (!data.datas_acrecimos) {
-                        li.classList.add('white');
-                    } else {
-                        li.classList.add('red');
-                    }
-
-                    lista.append(li);
-                })
-
+            const observer = new MutationObserver(mutation => {
                 marcacao(person);
+            });
 
-                const observer = new MutationObserver(mutation => { marcacao(person); });
+            observer.observe(calendario, {childList: true})
+        })
 
-                observer.observe(calendario, { childList: true })
-            })
-
-            const close = document.querySelector("#close");
-            close.addEventListener("click", function () {
-                const formulario = document.querySelector('form');
-                const lista      = document.querySelector('.lista');
-                formulario.classList.remove('none');
-                lista.classList.add('none');
-                close.classList.add('none');
-                window.location.href = urlbase;
-            })
-        })();
-    </script>
+        const close = document.querySelector("#close");
+        close.addEventListener("click", function () {
+            const formulario = document.querySelector('form');
+            const lista = document.querySelector('.lista');
+            formulario.classList.remove('none');
+            lista.classList.add('none');
+            close.classList.add('none');
+            window.location.href = urlbase;
+        })
+    })();
+</script>
 </body>
 
 </html>
